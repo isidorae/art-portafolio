67762cd748f4939727d2820f34d4ec6f8cee27f6
isidorae <mariaisidoraerrazuriz@gmail.com>
2025-02-21 16:44:22 -0300

    database update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,6 +17,14 @@ export default function Main() {
                     <Button className="text-white" as={ Link } to='/fineart-prints' variant="outline-light">Fine Art Prints</Button>
                 </div>
             </section>
+            <div>
+            <img src="https://assets.objkt.media/file/assets-003/KT1HtDvksXmgmk6H6vp48r1agxp772XLQgN1/62/thumb288" alt="gif"  
+                        width="220" 
+                        height="auto" 
+                        loading="lazy" 
+                        class="rounded mt-2"
+                        style={{ width: '100%', height: 'auto' }} />
+            </div>
         </div>
     )
 }
