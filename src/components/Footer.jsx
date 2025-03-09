1486390d0353cb9b618301e8bbff3c9a0dade4af
isidorae <mariaisidoraerrazuriz@gmail.com>
2025-03-09 16:17:06 -0300

    social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
 export default function Footer() { 
     return (
         <footer className="text-secondary d-flex flex-row flex-wrap justify-content-center align-items-center">
-            <span className="me-1">Made by Axis</span>
-            <span>© 2025</span>
+            <div>
+                <section className="d-flex flex-row align-items-center justify-content-center">
+                    <Link to="https://www.instagram.com/is_axis/" target="_blank"><i className="bi bi-instagram me-1"></i></Link>
+                    <Link to="https://www.x.com/isidoraxis/" target="_blank"><i className="bi bi-twitter-x"></i></Link>
+                </section>
+                <section>
+                    <span className="me-1">Made by Axis</span>
+                    <span>© 2025</span>
+                </section>
+            </div>
         </footer>
     )
 }
